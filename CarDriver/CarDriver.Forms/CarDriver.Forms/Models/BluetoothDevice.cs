1b1ff64acf9d01cec1d3b56d4b4b036a4ff4296e
Nod Caleb <nod.caleb@gmail.com>
2021-08-16 23:44:29 +0300

    Scanning for BT devices

---
+++ CarDriver/CarDriver.Forms/CarDriver.Forms/Models/BluetoothDevice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Plugin.BLE.Abstractions.Contracts;
+
+namespace CarDriver.Forms.Models
+{
+    public class BluetoothDevice
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName => Name ?? Id.ToString();
+        public IDevice Device { get; set; }
+
+        public BluetoothDevice()
+        {
+            
+        }
+
+        public BluetoothDevice(IDevice device)
+        {
+            Id = device.Id;
+            Name = device.Name;
+            Device = device;
+        }
+    }
+}
