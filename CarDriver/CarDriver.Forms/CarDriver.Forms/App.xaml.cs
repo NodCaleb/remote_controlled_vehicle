47dfd099bf28662d36633581450cc701233010ae
nod_caleb <nod.caleb@gmail.com>
2023-01-25 20:21:31 +0200

    One byte per motor data

--- CarDriver/CarDriver.Forms/CarDriver.Forms/App.xaml.cs
+++ CarDriver/CarDriver.Forms/CarDriver.Forms/App.xaml.cs
@@ -30,20 +30,39 @@ namespace CarDriver.Forms
         {
             if (CurrentBluetoothConnection?.ConnectionState == ConnectionState.Connected)
             {
-                var leftBytes = new byte[3];
-                var rightBytes = new byte[3];
+                //3 bytes approach
+                //var leftBytes = new byte[3];
+                //var rightBytes = new byte[3];
 
-                leftBytes[0] = (byte)'L';
-                rightBytes[0] = (byte)'R';
+                //leftBytes[0] = (byte)'L';
+                //rightBytes[0] = (byte)'R';
 
-                leftBytes[1] = (byte)(LeftPower > 0 ? 'F' : 'B');
-                rightBytes[1] = (byte)(RightPower > 0 ? 'F' : 'B');
+                //leftBytes[1] = (byte)(LeftPower > 0 ? 'F' : 'B');
+                //rightBytes[1] = (byte)(RightPower > 0 ? 'F' : 'B');
 
-                leftBytes[2] = (byte)(Math.Abs(LeftPower));
-                rightBytes[2] = (byte)(Math.Abs(RightPower));
+                //leftBytes[2] = (byte)(Math.Abs(LeftPower));
+                //rightBytes[2] = (byte)(Math.Abs(RightPower));
 
-                CurrentBluetoothConnection.Transmit(leftBytes);
-                CurrentBluetoothConnection.Transmit(rightBytes);
+                //1 byte approach
+
+                //Read power values
+                var leftValue = Math.Abs(LeftPower);
+                var rightValue = Math.Abs(RightPower);
+
+                //Reduce definition by 2 digits to contain motor index and direction
+                leftValue = leftValue >> 2;
+                rightValue = rightValue >> 2;
+
+                //First bit: 0 - left motor, 1 - right motor
+                leftValue = leftValue & 0b01111111;
+                rightValue = rightValue | 0b10000000;
+
+                //Second bit: 0 - forward, 1 - backward
+                leftValue = LeftPower > 0 ? leftValue & 0b10111111 : leftValue | 0b01000000;
+                rightValue = RightPower > 0 ? rightValue & 0b10111111 : rightValue | 0b01000000;
+
+                CurrentBluetoothConnection.Transmit(new[] { (byte)leftValue });
+                CurrentBluetoothConnection.Transmit(new[] { (byte)rightValue });
             }
 
             Thread.Sleep(100);
